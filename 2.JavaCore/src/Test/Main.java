35c05208ce94d48d96f7ab45a58c078f58232d9a
prostov <knockjkeee@gmail.com>
2017-04-10 17:34:27 +0300

    new add learn

--- 2.JavaCore/src/Test/Main.java
+++ 2.JavaCore/src/Test/Main.java
@@ -1,25 +1,10 @@
 package Test;
 
-import java.io.*;
-
 public class Main {
     public static void main(String[] args) {
-        StringBuilder bb = new StringBuilder();
-        String temp = null;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("D:\\Test\\1.txt")));
-            while ((temp = reader.readLine()) != null) {
-                bb.append(temp);
-                bb.append(" ");
-            }
+        String s = "name.1.part3.txt";
+        String temp = s.substring(s.length() - 3, s.length());
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        temp = String.valueOf(bb).trim();
         System.out.println(temp);
-
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task18.task1813;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+AmigoOutputStream
+*/
+
+public class AmigoOutputStream extends FileOutputStream {
+    private FileOutputStream stream;
+
+    public AmigoOutputStream(FileOutputStream stream) throws FileNotFoundException {
+        super(fileName);
+        this.stream = stream;
+    }
+
+    public static String fileName = "C:/tmp/result.txt";
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        stream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        stream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        stream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        flush();
+        String text = "JavaRush © All rights reserved.";
+        write(text.getBytes());
+        stream.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        stream.flush();
+    }
+}
