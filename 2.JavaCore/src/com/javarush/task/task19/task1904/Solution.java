dec2c309dfaf14995d4bc77831932e9b4c78b17e
prostov <knockjkeee@gmail.com>
2017-07-19 16:56:13 +0300

    new add learn

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -12,13 +12,13 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        try {
-            PersonScannerAdapter rr = new PersonScannerAdapter(new Scanner(new File("d:\\\\test/1.txt")));
-            Person pp = rr.read();
-            System.out.println(pp.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            PersonScannerAdapter rr = new PersonScannerAdapter(new Scanner(Paths.get("d:\\test\\1.txt")));
+//            Person pp = rr.read();
+//            System.out.println(pp.toString());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static class PersonScannerAdapter implements PersonScanner {
@@ -31,14 +31,36 @@ public class Solution {
 
         @Override
         public Person read() throws IOException {
-//            List<String> list = new ArrayList<>();
-//            fileScanner = new Scanner(new File("d://test/1.txt"));
-            Person test = null;
-            while (fileScanner.hasNext()) {
-                String temp = fileScanner.nextLine();
-                test = new Person(temp, temp, temp, new Date());
-            }
-            return test;
+//            Person person = null;
+//            Date birsday;
+//            while (fileScanner.hasNext()) {
+//                String[] temp = fileScanner.nextLine().split(" ");
+//                List<String> name = new ArrayList<>();
+//                List<Integer> date = new ArrayList<>();
+//                for (String s : temp) {
+//                    try {
+//                        date.add(Integer.parseInt(s));
+//                    } catch (NumberFormatException e) {
+//                        name.add(s);
+//                    }
+//                }
+//                birsday = new Date();
+//                birsday.setDate(date.get(0));
+//                birsday.setMonth(date.get(1) - 1);
+//                birsday.setYear(date.get(2) - 1900);
+//                person = new Person(name.get(1), name.get(2), name.get(0), birsday);
+//            }
+//            return person;
+
+            fileScanner = new Scanner(new File("matrix.txt"));
+            String line = fileScanner.nextLine();
+            String[] string = line.split(" ");
+            Date date = new Date(Integer.parseInt(string[3]),Integer.parseInt(string[4]),Integer.parseInt(string[5]));
+
+            return new Person(string[0],string [1], string[2],date);
+
+
+
         }
 
         @Override
@@ -46,6 +68,12 @@ public class Solution {
             fileScanner.close();
         }
     }
+}
 
-    }
+
+
+
+
+
+
 
